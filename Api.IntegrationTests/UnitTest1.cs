4a27260da83d9fb3836be4057f71556bef53992f
thuanlevan72 <88273694+thuanlevan72@users.noreply.github.com>
2026-04-12 09:02:06 +0700

    Update code

---
+++ Api.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace Api.IntegrationTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
